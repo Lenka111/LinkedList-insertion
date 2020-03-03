da8a179d29cdb1fac41e5f6fba62d0c4f0caaeb2
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-03-02 23:51:22 -0600

    Update IntegerNode.java

--- IntegerNode.java
+++ IntegerNode.java
@@ -4,10 +4,12 @@ public class IntegerNode {
     private Integer value;
     private IntegerNode next;
 
+    //the constructor
     public IntegerNode(Integer value) {
         this.value = value;
     }
 
+    //getters and setters
     public Integer getValue() {
         return value;
     }
@@ -24,6 +26,7 @@ public class IntegerNode {
         this.next = next;
     }
 
+    //string representation of the object
     public String toString() {
         return value.toString();
     }
